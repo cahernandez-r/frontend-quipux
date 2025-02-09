09bf346d79e614d43f7dcef462a8bb1c3c60c0ae
camilohr.dev <chernandez@unimayor.edu.co>
2025-02-08 19:26:09 -0500

    Add internazionalization and structure project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// import ngx-translate and the http loader
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        TranslateModule.forRoot({
+          loader: {
+              provide: TranslateLoader,
+              useFactory: httpTranslateLoader,
+              deps: [HttpClient]
+          },
+          defaultLanguage: 'es'
+        }),
+    ],
+    providers: [provideHttpClient(withInterceptorsFromDi())],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
